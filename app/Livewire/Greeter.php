5c29646bd6fa54e0f626472299e3aad200ca6ab7
roblebob <robert.koppisch@gmail.com>
2024-11-26 16:52:51 +0100

    I. The Fundamentals  2. Using Actions  DONE

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -2,13 +2,20 @@
 
 namespace App\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Greeter extends Component
 {
     public $name = 'John';
 
-    public function render()
+    public function changeName(string $newName): void
+    {
+        $this->name = $newName;
+    }
+
+
+    public function render() : View
     {
         return view('livewire.greeter');
     }
